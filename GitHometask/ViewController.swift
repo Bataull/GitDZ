24123622ffe7ce16b5b17cb0d850dbbcffa59163
Bataull <bataullinanton@gmail.com>
2023-11-29 14:22:50 +0300

    Second commit after copying

--- GitHometask/ViewController.swift
+++ GitHometask/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let label = UILabel()
-        label.text = "Commit after copying"
+        label.text = "Second Commit after copying"
         
         view.addSubview(label)
         
